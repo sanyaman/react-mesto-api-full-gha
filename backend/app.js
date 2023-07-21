099cb98a1fa3c38a84eb333d97fc1587c51ff227
sanyaman <119109046+sanyaman@users.noreply.github.com>
2023-07-21 21:36:02 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 require('dotenv').config();
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const { MESTODB = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const NOT_FOUND_ERROR = require('./errors/404');
