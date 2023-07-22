c9b3c1aa83972e731778b0e12fbe6758b2a56951
sanyaman <119109046+sanyaman@users.noreply.github.com>
2023-07-22 15:00:10 +0300

    sss

--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -8,7 +8,6 @@ const requestLogger = expressWinston.logger({
   format: winston.format.json(),
 });
 
-// логгер ошибок
 const errorLogger = expressWinston.errorLogger({
   transports: [
     new winston.transports.File({ filename: 'error.log' }),
