42346e156ae470b138372af63104bf00e5c659d2
sanyaman <alex4nd3r.l@yandex.ru>
2023-07-30 00:10:56 +0300

    хуяк и пофиксил

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -85,7 +85,7 @@ class Api {
 }
 
 export const api = new Api({
-  baseUrl: "http://localhost:3000/",
+  baseUrl: "http://api.domainsanyaman.nomoredomains.xyz/",
   headers: {
     'Content-Type': 'application/json'
   },
